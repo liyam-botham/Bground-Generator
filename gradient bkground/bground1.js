6c2ca7322c477ae46999df747b8276b037627bba
liyam-botham <64734428+liyam-botham@users.noreply.github.com>
2021-04-18 12:53:33 +0530

    Add files via upload

---
+++ gradient bkground/bground1.js
@@ -0,0 +1,16 @@
+var css1=document.getElementById("ipt1");
+var css2=document.getElementById("ipt2");
+var color1  =document.querySelector("h3");
+var body =document.getElementById("grad");
+
+function setGrad()
+{
+    body.style.background="linear-gradient(to right,"
+    + css1.value
+    +","
+    +css2.value
+    +")";
+    color1.textContent=body.style.background+";";
+}
+css1.addEventListener("input",setGrad);
+css2.addEventListener("input",setGrad);
